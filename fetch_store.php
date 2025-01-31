d8e664b0acd12bb0bf8c630eb77de4bf021e8ce0
ak-hila <148991941+ak-hila@users.noreply.github.com>
2025-01-31 12:54:08 +0300

    Update fetch_store.php

--- fetch_store.php
+++ fetch_store.php
@@ -14,13 +14,13 @@ if ($conn->connect_error) {
 }
 
 // Fetch store data
-$sql = "SELECT id, itemcode, itemname FROM 	store_material";
+$sql = "SELECT id, m_id, m_name FROM 	store_material";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table class='table table-bordered'><tr><th>ID</th><th>Item Code</th><th>Item Name</th></tr>";
     while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>{$row['id']}</td><td>{$row['itemcode']}</td><td>{$row['itemname']}</td></tr>";
+        echo "<tr><td>{$row['id']}</td><td>{$row['m_id']}</td><td>{$row['m_name']}</td></tr>";
     }
     echo "</table>";
 } else {
