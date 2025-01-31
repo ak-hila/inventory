657c1eb990e6faa077637787215f453c2fb8b136
ak-hila <148991941+ak-hila@users.noreply.github.com>
2025-01-31 13:44:43 +0300

    Update fetch_store.php

--- fetch_store.php
+++ fetch_store.php
@@ -7,7 +7,7 @@ $dbname = "db_mlab"; // Replace with your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+echo $conn;
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
